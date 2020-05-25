7c83a7adb8414c0850fd9d78564157eb0fb226ac
ccburst <ccburst123@gmail.com>
2020-05-25 19:52:58 +0900

    0525

--- read_adc_value.ino
+++ read_adc_value.ino
@@ -1,52 +1,75 @@
-    #include <Wire.h>
-
+#include <Wire.h>
 #define Addr 0x50 //  I2C address
 int data;
 
 void setup()
-{ 
-  Wire.begin(); 
+{
+  Wire.begin();
   Serial.begin(9600);
   Serial.println("start!");
 
   REG_write(0x11, 0x01);
   REG_write(0x00, 0x00);
-  REG_write(0x01, 0x0e);
-  REG_write(0x02, 0x00);
-  REG_write(0x03, 0x00); // 選擇通道
-  REG_write(0x07, 0x04);
+  
+  // 0x01 0x02 為PGA暫存器 用於差分輸入信號進行信號放大及∆𝐷𝐼±控制
+  REG_write(0x01, 0x00); // VREFGN=1 , ADGN=1 , PGAGN=1
+  REG_write(0x02, 0x00); // DCSET=+0V
+ 
+  REG_write(0x03, 0x00); // 選擇通道 CH1為反相輸入端 CH0為正相輸入端
+  REG_write(0x07, 0x06); // VREFS=1 調整為外部參考電壓比較
   REG_write(0x09, 0x1f);
-  REG_write(0x08, 0x00);  
+  REG_write(0x08, 0x00);
+  
 }
 
 void loop()
 {
   delay(1000);
-  do{
-    Serial.print("---------"); 
-    REG_read(0x08);
-    delay(1000);
-  }  while((data&2)==0);  // 檢測ADCR1中EOC位  
 
-  Read_AD_Value();  
+  Serial.println("檢測EOC"); 
+  do{
+      
+    REG_read(0x08);
+    Serial.print("0x08="); 
+    Serial.println(data); 
+
+    delay(1000);
+    
+  }  while((data&2)==0);  // 檢測ADCR1中EOC位
+  
+  Serial.println("EOC為1"); 
+
+  Read_AD_Value();
+  
 }
 
 void REG_write(int a,int b){
+
   Wire.beginTransmission(Addr);
   Wire.write(a);  // 進入 暫存器
   Wire.write(b);  // 寫入控制內容
+  //Serial.println("write sucess");
+
   Wire.endTransmission();  // Stop I2C Transmission
+
 }
 
 void REG_read(int a){
+  
   Wire.beginTransmission(Addr);
   Wire.write(a);  // 進入 暫存器
-  Wire.endTransmission(0);  // Stop I2C Transmission  
+
+  Wire.endTransmission(0);  // Stop I2C Transmission
+  
   Wire.requestFrom(Addr, 1);
+
   if (Wire.available())
   {
     data = Wire.read();
+    //Serial.println(data);   //顯示暫存器的內部狀態
   }
+  
+  
 }
 
 void Read_AD_Value(void) 
@@ -75,19 +98,20 @@ void Read_AD_Value(void)
   ADC_data=(SI_I/VR_I)*K;
 
   if(value >> 23 ==0){
-    Serial.print("value=");
+    Serial.println("正數");
     Serial.println(value);
     ADC_value=(float)value*(3.3/8388608);
-    Serial.print("ADC_value=+");
+    Serial.print("ADC_value=");
     Serial.println(ADC_value);
   }
   else{
+    Serial.println("負數");
     ADC_value=( (value^0x00FFFFFF) +1);
-    Serial.print("value=");
     Serial.println(value);
+    Serial.println(ADC_value);
     ADC_value=(float)ADC_value*(3.3/8388608);
     Serial.print("ADC_value=-");
     Serial.println(ADC_value);
-  }
-
+  } 
+ 
 }
